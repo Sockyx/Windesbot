0c60378ae2862d8def18beea7b1b6c9f2f5856d0
Lucas Ouwens <lucasouwens@gmail.com>
2018-10-07 19:28:07 +0200

    This one is also not supposed to get deleted >.>

---
+++ src/main/java/com/windesheim/constant/BotConstant.java
@@ -0,0 +1,25 @@
+package com.windesheim.constant;
+
+/**
+ * Bot constants
+ *
+ * @author Lucas Ouwens
+ */
+public class BotConstant {
+
+    /**
+     * A string which contains the bot token for the Windesbot
+     */
+    public static final String discordBotToken = "<discord bot token>";
+
+    /**
+     * Permission level for the bot, used for administrative commands.
+     */
+    public static final int discordBotPermissionLevel = 8;
+
+    /**
+     * Prefix for bot commands, TODO allow discords to set their own.
+     */
+    public static final String windesbotCommandPrefix = "w!";
+
+}
