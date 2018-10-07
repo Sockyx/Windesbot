062886391c99c948ab7aad2ba8496834da1d1756
Lucas Ouwens <lucasouwens@gmail.com>
2018-10-07 19:26:54 +0200

    Constants aren't supposed to be deleted

---
+++ src/main/java/com/windesheim/constant/DatabaseConstant.java
@@ -0,0 +1,30 @@
+package com.windesheim.constant;
+
+/**
+ * Constants for the database connection
+ *
+ * @author Lucas Ouwens
+ */
+public class DatabaseConstant {
+
+    public static final String databaseServer = "localhost";
+    public static final int databasePort = 3306;
+
+    public static final String databaseType = "mysql";
+
+    public static final String databaseUsername = "<username>";
+    public static final String databasePassword = "<password>";
+
+    public static final String databaseName = "windesbot";
+
+    public static final String cachePrepareStatements = "true";
+
+    public static final String cachePrepareStatementSize = "250";
+
+    public static final String prepareStatementSQLLimit = "2048";
+
+    // somehow does not work without this
+    public static final String getters = "?useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+
+}
